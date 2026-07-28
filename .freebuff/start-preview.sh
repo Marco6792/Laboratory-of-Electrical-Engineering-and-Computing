9352f5c52fa86c8dabc1093cb553c3cd216d2a54
Brilydal123 <admin@brilydal.onmicrosoft.com>
2026-07-28 21:56:11 +0100

    Remove .freebuff from tracking

--- .freebuff/start-preview.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/marco/Desktop/laboratory-of-electrical-eng/leec-web
-npx next dev -p 3000 > /home/marco/Desktop/laboratory-of-electrical-eng/.freebuff/preview-thms4rnzo1mniy.log 2>&1
